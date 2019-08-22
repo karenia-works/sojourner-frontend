27ff2a1c74f1bf08a103959299c49ec4a26109fa
Rynco Maekawa <lynzrand@outlook.com>
2019-08-22 16:21:18 +0800

    Add font from Google Fonts

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,7 @@
 </template>
 <style lang="stylus">
 #app
-  font-family 'Avenir', Helvetica, Arial, sans-serif
+  font-family 'Barlow Condensed', Helvetica, Arial, sans-serif
   -webkit-font-smoothing antialiased
   -moz-osx-font-smoothing grayscale
   text-align center
