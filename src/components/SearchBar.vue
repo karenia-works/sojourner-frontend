72aff8029f170da96ce0d2a86ecca227013327c1
Rynco Maekawa <lynzrand@outlook.com>
2019-08-23 16:22:02 +0800

    Move btn and textbox into style

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="searchbar">
+    <input type="text" class="input" id="search" />
+    <button id="search-btn" class="btn search-btn">Search</button>
+  </div>
+</template>
+
+<style lang="stylus">
+.searchbar {
+  display: flex
+  flex-direction: row
+  justify-content: flex-start
+}
+</style>
