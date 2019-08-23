94b750fe7ac4b2168428b9fdea76366961b0818c
Rynco Maekawa <lynzrand@outlook.com>
2019-08-23 16:21:01 +0800

    Clean homepage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,31 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <div class="jumbotron">
+      <h1>Arriving Alone?</h1>
+    </div>
+    <search-bar></search-bar>
   </div>
 </template>
+
+<style lang="stylus" scoped>
+.jumbotron {
+  h1 {
+    font-size: 72px
+    font-weight: normal
+    text-transform: uppercase
+  }
+}
+</style>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import SearchBar from "@/components/SearchBar.vue";
 
 @Component({
   components: {
-    HelloWorld
+    HelloWorld,
+    SearchBar
   }
 })
 export default class Home extends Vue {}
