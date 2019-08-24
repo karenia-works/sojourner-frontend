be65ea295210ba3728c6ef58f8287178a87b7bc3
Rynco Maekawa <lynzrand@outlook.com>
2019-08-24 09:17:38 +0800

    Add dependency mdi-vue

--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import NotFoundView from './views/NotFound.vue'
 
 Vue.use(Router)
 
@@ -51,6 +52,10 @@ export default new Router({
           component: () => import('./views/room/RoomSubmit.vue')
         }
       ]
+    },
+    {
+      path: '/**',
+      component: NotFoundView
     }
   ]
 })
