b16c4610054ab3739c652d2b9c0c7a9558c4fbe1
Rynco Maekawa <lynzrand@outlook.com>
2019-08-25 15:28:06 +0800

    Format file

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,75 +1,95 @@
 <template>
-    <div v-if="showLogin">
-      <div class="cover"  @click="closeLogin"></div>
-      <div class="login loginWindow" v-if="!signup">
-        <!-- <close-icon class="close" @click="closeLogin"/> -->
-        <input type="email" class="input" id="email" placeholder="email address" v-model.trim="email" :class="{textErr: !inputCheck.email}" autofocus />
-        <input type="password" class="input" id="password" placeholder="password" v-model="password" />
-        <div class="btnDiv">
-          <button id="login-btn" class="btn" @click="submitLogin">Login</button>
-        </div>
-        <p class="signupLink">
-            Don't have an account?&nbsp;
-            <span class="jumpLink" @click="toSignup">Sign up</span> now
-        </p>
+  <div v-if="showLogin">
+    <div class="cover" @click="closeLogin"></div>
+    <div class="login loginWindow" v-if="!signup">
+      <!-- <close-icon class="close" @click="closeLogin"/> -->
+      <input
+        type="email"
+        class="input"
+        id="email"
+        placeholder="email address"
+        v-model.trim="email"
+        :class="{textErr: !inputCheck.email}"
+        autofocus
+      />
+      <input type="password" class="input" id="password" placeholder="password" v-model="password" />
+      <div class="btnDiv">
+        <button id="login-btn" class="btn" @click="submitLogin">Login</button>
       </div>
-      <div class="signup loginWindow" v-if="signup">
-        <!-- <close-icon class="close" @click="closeLogin"/> -->
-        <input type="email" class="input" id="email" placeholder="email address" v-model.trim="email" :class="{textErr: !inputCheck.email}" autofocus />
-        <input type="password" class="input" id="password" placeholder="password" v-model="password" />
-        <input type="text" class="input" id="name" placeholder="full name" v-model.trim="name" />
-        <input type="tel" class="input" id="phone" placeholder="phone number" v-model.trim="phone" :class="{textErr: !inputCheck.phone}" />
-        <div class="btnDiv">
-          <button id="login-btn" class="btn" @click="submitSignup">Signup</button>
-        </div>
-        <p class="signupLink">
-            Already have an account?&nbsp;
-            <span class="jumpLink" @click="toLogin">Log in</span>
-        </p>
-      </div>
+      <p class="signupLink">
+        Don't have an account?&nbsp;
+        <span class="jumpLink" @click="toSignup">Sign up</span> now
+      </p>
     </div>
+    <div class="signup loginWindow" v-if="signup">
+      <!-- <close-icon class="close" @click="closeLogin"/> -->
+      <input
+        type="email"
+        class="input"
+        id="email"
+        placeholder="email address"
+        v-model.trim="email"
+        :class="{textErr: !inputCheck.email}"
+        autofocus
+      />
+      <input type="password" class="input" id="password" placeholder="password" v-model="password" />
+      <input type="text" class="input" id="name" placeholder="full name" v-model.trim="name" />
+      <input
+        type="tel"
+        class="input"
+        id="phone"
+        placeholder="phone number"
+        v-model.trim="phone"
+        :class="{textErr: !inputCheck.phone}"
+      />
+      <div class="btnDiv">
+        <button id="login-btn" class="btn" @click="submitSignup">Signup</button>
+      </div>
+      <p class="signupLink">
+        Already have an account?&nbsp;
+        <span class="jumpLink" @click="toLogin">Log in</span>
+      </p>
+    </div>
+  </div>
 </template>
 
 <style lang="stylus" scoped>
 .cover {
-  height 100%
-  width 100%
-  position absolute
-  top 0
-  left 0
-  background-color rgba(0, 0, 0, 0.5)
-  z-index 8
+  height: 100%
+  width: 100%
+  position: absolute
+  top: 0
+  left: 0
+  background-color: rgba(0, 0, 0, 0.5)
+  z-index: 8
 }
 
 .loginWindow {
-  width 400px
-  background-color colors.bg-light
-  border-radius 5px   
-
-  position absolute 
-  left 50%
-  margin-left -200px
-  z-index 9
-
-  display flex
-  flex-direction column
-  // justify-content center
-  align-items center
-  padding spaces._6
+  width: 400px
+  background-color: colors.bg-light
+  border-radius: 5px
+  position: absolute
+  left: 50%
+  margin-left: -200px
+  z-index: 9
+  display: flex
+  flex-direction: column
+  align-items: center
+  padding: spaces._6
 }
 
 .login {
-  top 25%
+  top: 25%
 }
 
 .signup {
-  top 15%
+  top: 15%
 }
 
 .btnDiv {
-  height 40px
-  margin-top 15px
-  margin-bottom 10px
+  height: 40px
+  margin-top: 15px
+  margin-bottom: 10px
 }
 
 .loginWindow input {
@@ -78,19 +98,17 @@
 }
 
 input.textErr {
-  // color: colors.error
-  background-color colors.accent
+  background-color: colors.accent
 }
 
 .signupLink {
-  color colors.text-medium
+  color: colors.text-medium
 }
 
 .jumpLink {
-  text-decoration none 
+  text-decoration: none
   color: colors.accent
 }
-
 </style>
 
 <script lang="ts">
@@ -103,75 +121,70 @@ import RoomInfo from "@/components/RoomInfo.vue"; // @ is an alias to /src
   }
 })
 export default class Login extends Vue {
-@PropSync('show', {default: false, type: Boolean})
-showLogin!:boolean;
-signup = false;
+  @PropSync("show", { default: false, type: Boolean })
+  showLogin!: boolean;
+  signup = false;
 
-openLogin() {
-  this.showLogin = true;
-}
-closeLogin() {
-  this.showLogin = false;
-}
-toLogin() {
-  this.signup = false;
-}
-toSignup() {
-  this.signup = true;
-}
-
-inputCheck = {
-  email: true,
-  password: true,
-  name: true,
-  phone: true
-}
-
-@Watch('email')
-onEmailChanged(val: string, oldVal: string) {
-  if (val)
-    this.inputCheck.email = this.patterns.email.test(val);
-  else 
-    this.inputCheck.email = true;
-}
-
-@Watch('phone')
-onPhoneChanged(val: string, oldVal: string) {
-  if (val)
-    this.inputCheck.phone = this.patterns.phone.test(val);
-  else 
-    this.inputCheck.phone = true;
-}
-
-patterns = {
-  email: /^[a-zA-Z0-9_-]+@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)+$/,
-  phone: /^1[3456789]\d{9}$/
-}
-
-email = '';
-password = '';
-name = '';
-phone = '';
-
-submitLogin() {
-  var check = this.inputCheck;
-  if (!check.email) {
-    alert("Please check your email address");
-  } else {
-    alert("loading")
+  openLogin() {
+    this.showLogin = true;
   }
-}
-
-submitSignup() {
-  var check = this.inputCheck;
-  if (!check.email) {
-    alert("Please check your email address");
-  } else if (!check.phone) {
-    alert("Please check your phone number");
-  } else {
-    alert("loading")
+  closeLogin() {
+    this.showLogin = false;
   }
-}
+  toLogin() {
+    this.signup = false;
+  }
+  toSignup() {
+    this.signup = true;
+  }
 
+  inputCheck = {
+    email: true,
+    password: true,
+    name: true,
+    phone: true
+  };
+
+  @Watch("email")
+  onEmailChanged(val: string, oldVal: string) {
+    if (val) this.inputCheck.email = this.patterns.email.test(val);
+    else this.inputCheck.email = true;
+  }
+
+  @Watch("phone")
+  onPhoneChanged(val: string, oldVal: string) {
+    if (val) this.inputCheck.phone = this.patterns.phone.test(val);
+    else this.inputCheck.phone = true;
+  }
+
+  patterns = {
+    email: /^[a-zA-Z0-9_-]+@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)+$/,
+    phone: /^1[3456789]\d{9}$/
+  };
+
+  email = "";
+  password = "";
+  name = "";
+  phone = "";
+
+  submitLogin() {
+    var check = this.inputCheck;
+    if (!check.email) {
+      alert("Please check your email address");
+    } else {
+      alert("loading");
+    }
+  }
+
+  submitSignup() {
+    var check = this.inputCheck;
+    if (!check.email) {
+      alert("Please check your email address");
+    } else if (!check.phone) {
+      alert("Please check your phone number");
+    } else {
+      alert("loading");
+    }
+  }
 }
 </script>
