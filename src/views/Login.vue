d09f8b649cd9d3be64d408868e68ce9d4232e824
wsy <sywang1207@gmail.com>
2019-08-25 14:58:09 +0800

    add signup module, email&phone check

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,16 +1,30 @@
 <template>
     <div v-if="showLogin">
       <div class="cover"  @click="closeLogin"></div>
-      <div class="login">
+      <div class="login loginWindow" v-if="!signup">
         <!-- <close-icon class="close" @click="closeLogin"/> -->
-        <input type="text" class="input" id="email" placeholder="email address" v-model.trim="email" />
+        <input type="email" class="input" id="email" placeholder="email address" v-model.trim="email" :class="{textErr: !inputCheck.email}" autofocus />
         <input type="password" class="input" id="password" placeholder="password" v-model="password" />
         <div class="btnDiv">
           <button id="login-btn" class="btn ">Login</button>
         </div>
-        <p class="signup">
-            Don't have an account? 
-            <a href="/register" class="signupLink">Sign up</a> now
+        <p class="signupLink">
+            Don't have an account?&nbsp;
+            <span class="jumpLink" @click="toSignup">Sign up</span> now
+        </p>
+      </div>
+      <div class="signup loginWindow" v-if="signup">
+        <!-- <close-icon class="close" @click="closeLogin"/> -->
+        <input type="email" class="input" id="email" placeholder="email address" v-model.trim="email" :class="{textErr: !inputCheck.email}" autofocus />
+        <input type="password" class="input" id="password" placeholder="password" v-model="password" />
+        <input type="text" class="input" id="name" placeholder="full name" v-model.trim="name" />
+        <input type="tel" class="input" id="phone" placeholder="phone number" v-model.trim="phone" :class="{textErr: !inputCheck.phone}" />
+        <div class="btnDiv">
+          <button id="login-btn" class="btn ">Signup</button>
+        </div>
+        <p class="signupLink">
+            Already have an account?&nbsp;
+            <span class="jumpLink" @click="toLogin">Log in</span>
         </p>
       </div>
     </div>
@@ -27,14 +41,12 @@
   z-index 8
 }
 
-.login {
-  // height 300px
+.loginWindow {
   width 400px
   background-color colors.bg-light
   border-radius 5px   
 
   position absolute 
-  top 25%
   left 50%
   margin-left -200px
   z-index 9
@@ -46,30 +58,43 @@
   padding spaces._6
 }
 
+.login {
+  top 25%
+}
+
+.signup {
+  top 15%
+}
+
 .btnDiv {
   height 40px
   margin-top 15px
   margin-bottom 10px
 }
 
-.signup {
-  color colors.text-medium
+.loginWindow input {
+  width: 100%
+  margin-v: spaces._3
+}
+
+input.textErr {
+  // color: colors.error
+  background-color colors.accent
 }
 
 .signupLink {
-  text-decoration none 
-  color: colors.accent
+  color colors.text-medium
 }
 
-#email, #password{
-  width: 100%
-  margin-v: spaces._3
+.jumpLink {
+  text-decoration none 
+  color: colors.accent
 }
 
 </style>
 
 <script lang="ts">
-import { Component, Prop,PropSync, Vue } from "vue-property-decorator";
+import { Component, Prop, PropSync, Vue, Watch } from "vue-property-decorator";
 import RoomInfo from "@/components/RoomInfo.vue"; // @ is an alias to /src
 
 @Component({
@@ -78,18 +103,55 @@ import RoomInfo from "@/components/RoomInfo.vue"; // @ is an alias to /src
   }
 })
 export default class Login extends Vue {
-  @PropSync('show', {default: false, type: Boolean})
-  showLogin!:boolean;
+@PropSync('show', {default: false, type: Boolean})
+showLogin!:boolean;
+signup = false;
 
-  openLogin() {
-    this.showLogin = true;
-  }
-  closeLogin() {
-    this.showLogin = false;
-  }
+openLogin() {
+  this.showLogin = true;
+}
+closeLogin() {
+  this.showLogin = false;
+}
+toLogin() {
+  this.signup = false;
+  this.inputCheck.name = true;
+  this.inputCheck.phone = true;
+}
+toSignup() {
+  this.signup = true;
+  if (this.phone)
+    this.inputCheck.phone = this.patterns.phone.test(this.phone);
+}
 
-  email = '';
-  password = '';
+inputCheck = {
+  email: true,
+  password: true,
+  name: true,
+  phone: true
+}
+
+@Watch('email')
+onEmailChanged(val: string, oldVal: string) {
+  if (val)
+    this.inputCheck.email = this.patterns.email.test(val);
+}
+
+@Watch('phone')
+onPhoneChanged(val: string, oldVal: string) {
+  if (val)
+    this.inputCheck.phone = this.patterns.phone.test(val);
+}
+
+patterns = {
+  email: /^[a-zA-Z0-9_-]+@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)+$/,
+  phone: /^1[3456789]\d{9}$/
+}
+
+email = '';
+password = '';
+name = '';
+phone = '';
 
 }
 </script>
