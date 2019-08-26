07c4566748f298d91dd4685ab88c588792981aa3
Rynco Maekawa <lynzrand@outlook.com>
2019-08-26 18:45:06 +0800

    Add admin pages

--- src/router.ts
+++ src/router.ts
@@ -4,6 +4,8 @@ import Home from './views/Home.vue'
 import NotFoundView from './views/NotFound.vue'
 import Login from './views/Login.vue'
 import Room from './views/Room.vue'
+import Userland from './views/Userland.vue'
+
 Vue.use(Router)
 
 export default new Router({
@@ -11,85 +13,103 @@ export default new Router({
   base: process.env.BASE_URL,
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: Home,
-    },
-    {
-      path: '/s',
-      name: 'search',
-      component: () => import('./views/Search.vue'),
-    },
-    {
-      path: '/login',
-      name: 'login',
-      component: Login,
-      props: {
-        isSinglePage: true,
-        show: true,
-        isSignUp: false,
-      },
-    },
-    {
-      path: '/register',
-      name: 'register',
-      component: Login,
-      props: {
-        isSinglePage: true,
-        show: true,
-        isSignUp: true,
-      },
-    },
-    {
-      path: '/u',
-      name: 'user',
+      path: '/admin',
+      // name: 'adminWorld',
+      component: () => import('./views/admin/App.vue'),
       children: [
         {
-          path: '/me',
-          name: 'user_me',
-          component: () => import('./views/user/Me.vue'),
+          path: '',
+          name: 'adminHomepage',
+          component: () => import('./views/admin/Home.vue'),
         },
       ],
     },
     {
-      path: '/ord',
-      name: 'orders',
-      component: () => import('./views/user/Orders.vue')
-    },
-    {
-      path: '/detail',
-      name: 'detail',
-      component: () => import('./views/room/HouseDetail.vue'),
-    },
-    {
-      path: '/r/:id',
-      component: Room,
+      path: '/',
+      component: Userland,
       children: [
         {
-          path: 'detail',
-          name: 'room_detail',
+          path: '',
+          name: 'home',
+          component: Home,
+        },
+        {
+          path: '/s',
+          name: 'search',
+          component: () => import('./views/Search.vue'),
+        },
+        {
+          path: '/login',
+          name: 'login',
+          component: Login,
+          props: {
+            isSinglePage: true,
+            show: true,
+            isSignUp: false,
+          },
+        },
+        {
+          path: '/register',
+          name: 'register',
+          component: Login,
+          props: {
+            isSinglePage: true,
+            show: true,
+            isSignUp: true,
+          },
+        },
+        {
+          path: '/u',
+          name: 'user',
+          children: [
+            {
+              path: '/me',
+              name: 'user_me',
+              component: () => import('./views/user/Me.vue'),
+            },
+          ],
+        },
+        {
+          path: '/ord',
+          name: 'orders',
+          component: () => import('./views/user/Orders.vue'),
+        },
+        {
+          path: '/detail',
+          name: 'detail',
           component: () => import('./views/room/HouseDetail.vue'),
         },
         {
-          path: 'submit',
-          name: 'room_submit',
-          component: () => import('./views/room/PayCheck.vue'),
+          path: '/r/:id',
+          component: Room,
+          children: [
+            {
+              path: 'detail',
+              name: 'room_detail',
+              component: () => import('./views/room/HouseDetail.vue'),
+            },
+            {
+              path: 'submit',
+              name: 'room_submit',
+              component: () => import('./views/room/PayCheck.vue'),
+            },
+            {
+              path: 'shortpay',
+              name: 'short_term_pay',
+              component: () => import('./views/room/ShortTermPay.vue'),
+            },
+            {
+              path: 'longpay',
+              name: 'long_term_pay',
+              component: () => import('./views/room/LongTermPay.vue'),
+            },
+          ],
         },
         {
-          path: 'shortpay',
-          name: 'short_term_pay',
-          component: () => import('./views/room/ShortTermPay.vue'),
-        },
-        {
-          path: 'longpay',
-          name: 'long_term_pay',
-          component: () => import('./views/room/LongTermPay.vue'),
+          path: '/**',
+          component: NotFoundView,
         },
       ],
-    },
-    {
-      path: '/**',
-      component: NotFoundView,
     },
   ],
   scrollBehavior(to, from, savedPosition) {
---
+++ src/views/admin/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <!-- Root page for admin pages -->
+  <div class="admin-world">
+    <!-- <navbar></navbar> -->
+    <router-view />
+  </div>
+</template>
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component({
+  components: {  }
+})
+export default class AdminWorld extends Vue {}
+</script>
+
+<style lang="stylus">
+#userland {
+  color: colors-admin.text-dark
+}
+
+body {
+  background: colors-admin.bg-light
+}
+</style>
