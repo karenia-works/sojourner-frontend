5d31b0aee1e643bf278e36461811aae5ee5eed38
“MaksimXing <henryx@buaa.edu.cn>
2019-08-27 16:56:31 +0800

    merge stylus and router add

---
+++ src/components/NavbarAdmin.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="navbar">
+    <div class="nav-left">
+      <router-link to="/">
+        <div class="nav-el logo">SOJOURNER</div>
+      </router-link>
+      <router-link to="/s">
+        <div class="nav-el">Search</div>
+      </router-link>
+      <router-link to="/">
+        <div class="nav-el">Explore</div>
+      </router-link>
+    </div>
+    <div class="nav-right">
+      <div class="nav-el" id="sign_up" @click="switchLogin()">Sign up</div>
+      <login :show.sync="showLogin" />
+    </div>
+  </div>
+</template>
+
+<style lang="stylus" scoped>
+.navbar {
+  background: var(--color-bg-medium)
+  display: flex
+  flex-direction: row
+  justify-content: space-between
+  padding: 16px
+  padding-h: 24px
+  height: 48px
+  font-family: fonts-title
+  font-size: font-sizes.body-larger
+
+  .nav-left {
+    display: flex
+    flex-direction: row
+    align-items: stretch
+  }
+
+  .nav-right {
+    display: flex
+    flex-direction: row
+    align-items: stretch
+  }
+
+  .nav-el {
+    display: flex
+    align-items: center
+    margin-h: 8px
+    padding-h: 16px
+  }
+}
+
+.logo {
+  font-weight: bold
+}
+</style>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import Login from "@/views/Login.vue";
+
+@Component({ components: { Login } })
+export default class Navbar extends Vue {
+  showLogin: boolean = false;
+
+  switchLogin() {
+    this.showLogin = !this.showLogin;
+  }
+}
+</script>
--- src/views/admin/admin-router.ts
+++ src/views/admin/admin-router.ts
@@ -6,4 +6,24 @@ export const adminRoutes: RouteConfig[] = [
     name: 'adminHomepage',
     component: () => import('@/views/admin/Home.vue'),
   },
+  {
+    path: 'manageUser',
+    name: 'manageUser',
+    component: () => import('@/views/admin/ManageUser.vue'),
+  },
+  {
+    path: 'ManageOrder',
+    name: 'ManageOrder',
+    component: () => import('@/views/admin/ManageOrder.vue'),
+  },
+  {
+    path: 'ManageRoom',
+    name: 'ManageRoom',
+    component: () => import('@/views/admin/ManageRoom.vue'),
+  },
+  {
+    path: 'ManageRepair',
+    name: 'ManageRepair',
+    component: () => import('@/views/admin/ManageRepair.vue'),
+  },
 ]
