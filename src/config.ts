6a4f145f5f6d9415c87d993290ebdbb6e57a36ff
Rynco Maekawa <lynzrand@outlook.com>
2019-08-29 23:02:34 +0800

    .

--- src/config.ts
+++ src/config.ts
@@ -1,15 +1,17 @@
 export default {
   backend: {
-    address: 'https://sojourner.rynco.me/api/v1',
-    tokenEndpoint: '/auth/login',
-    userEndpoint: '/user',
-    searchEndpoint: '/room',
-    imageEndpoint: '/image',
-    roomEndpoint: (id: string) => `/room/${id}`,
-    orderEndpoint: (id: string) => `/order/${id}`,
+    address: 'https://sojourner.rynco.me/api/v1/',
+    tokenEndpoint: 'https://sojourner.rynco.me/connect/token',
+    userEndpoint: 'user',
+    searchEndpoint: 'room',
+    imageEndpoint: 'image',
+    issueEndpoint: 'issue',
+    roomEndpoint: (id: string) => `room/${id}`,
+    orderEndpoint: (id: string) => `order/${id}`,
+    descreteIssueEndpoint: (id: string) => `issue/${id}`
   },
   auth: {
-    client_id: '',
-    client_secret: '',
-  },
+    client_id: 'client',
+    client_secret: 'client'
+  }
 }
