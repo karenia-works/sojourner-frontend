6900891fc982b84b08cd7a59fb3f794c6ae173b5
Rynco Maekawa <lynzrand@outlook.com>
2019-08-31 00:59:13 +0800

    Fix auth data storage

--- src/store/user.ts
+++ src/store/user.ts
@@ -48,17 +48,17 @@ interface UserLoginData {
 }
 
 const formHeaders = {
-  'Content-Type': 'application/x-www-form-urlencoded'
+  'Content-Type': 'application/x-www-form-urlencoded',
 }
 
 export var getters: GetterTree<UserState, RootState> = {
   authHeader(state) {
     if (state.loggedIn && state.userLoginData) {
       return {
-        Authorization: `Bearer ${state.userLoginData.access_token}`
+        Authorization: `Bearer ${state.userLoginData.access_token}`,
       }
     } else return undefined
-  }
+  },
 }
 
 export var actions: ActionTree<UserState, RootState> = {
@@ -76,14 +76,14 @@ export var actions: ActionTree<UserState, RootState> = {
       client_secret: config.auth.client_secret,
       grant_type: 'password',
       username: email,
-      password
+      password,
     }
 
     let loginData = await axios.post<UserLoginData>(
       new URL(config.backend.tokenEndpoint).href,
       qs.stringify(tokenCtx),
       {
-        headers: formHeaders
+        headers: formHeaders,
       }
     )
 
@@ -91,20 +91,21 @@ export var actions: ActionTree<UserState, RootState> = {
     if (!isLoginSuccessful) return
 
     ctx.commit('updateLoginData', { data: loginData.data, email })
+
+    await ctx.dispatch('updateProfile')
+
     ctx.commit('tryStoreData')
 
     let meData = await axios.get(
       new URL('user/me', config.backend.address).href,
       {
         headers: {
-          Authorization: `Bearer ${loginData.data.access_token}`
-        }
+          Authorization: `Bearer ${loginData.data.access_token}`,
+        },
       }
     )
 
     ctx.state.role = meData.data.role
-
-    await ctx.dispatch('updateProfile')
 
     if (payload.callback) {
       payload.callback(isLoginSuccessful)
@@ -134,10 +135,10 @@ export var actions: ActionTree<UserState, RootState> = {
         password: payload.password,
         role: 'IdentityServerAccessToken',
         id: null,
-        key: null
+        key: null,
       }
     )
-  }
+  },
 }
 
 export var mutations: MutationTree<UserState> = {
@@ -151,24 +152,23 @@ export var mutations: MutationTree<UserState> = {
     state.loggedIn = true
   },
   tryRestoreData(state) {
-    let header = window.localStorage.getItem('auth')
-    let scope = window.localStorage.getItem('auth_scope') || 'identityServerApi'
-    let email = window.localStorage.getItem('email') || 'identityServerApi'
-    if (header && scope && email) {
-      state.userLoginData = {
-        access_token: header,
-        expires_in: 0,
-        token_type: 'Bearer',
-        scope
-      }
+    let email = window.localStorage.getItem('email') || ''
+    let profile = JSON.parse(window.localStorage.getItem('profile') || '')
+    let authData = JSON.parse(window.localStorage.getItem('authData') || '')
+    if (email && profile && authData) {
       state.email = email
+      state.profile = profile
+      state.userLoginData = authData
       state.loggedIn = true
     }
   },
   tryStoreData(state) {
     if (state.userLoginData && state.email) {
-      window.localStorage.setItem('auth', state.userLoginData.access_token)
-      window.localStorage.setItem('auth_scope', state.userLoginData.scope)
+      window.localStorage.setItem(
+        'authData',
+        JSON.stringify(state.userLoginData)
+      )
+      window.localStorage.setItem('profile', JSON.stringify(state.profile))
       window.localStorage.setItem('email', state.email)
     }
   },
@@ -178,7 +178,11 @@ export var mutations: MutationTree<UserState> = {
     state.loggedIn = false
     state.loginError = undefined
     state.email = undefined
-  }
+
+    window.localStorage.removeItem('authData')
+    window.localStorage.removeItem('profile')
+    window.localStorage.removeItem('email')
+  },
 }
 
 export class UserState {
@@ -194,5 +198,5 @@ export const userStore: Module<UserState, RootState> = {
   state: new UserState(),
   mutations,
   actions,
-  getters
+  getters,
 }
